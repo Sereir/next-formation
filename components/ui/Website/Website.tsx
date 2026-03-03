e1d62124798a4b13339be552589579d45c25ab94
NSEREIR <nacim.sereir@ynov.com>
2026-03-03 09:27:09 +0100

    :)

--- components/ui/Website/Website.tsx
+++ components/ui/Website/Website.tsx
@@ -5,7 +5,8 @@ export default function Website({ title, thumbnail, slug }: WebsiteType) {
   return (
     <Link href={`websites/${slug}`}>
       <div>
-        {thumbnail} {title}
+        <div className="text-xl font-bold ">{title}</div>
+        <img src={`/websites/${thumbnail}`} alt={title} />
       </div>
     </Link>
   );
